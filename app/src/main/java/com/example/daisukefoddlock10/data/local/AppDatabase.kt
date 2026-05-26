e149dc6339b5a595d5dea17f291885651f74f991
Radit Alfa Anugerah Bombing <radit.bombing@gmail.com>
2026-05-26 14:39:29 +0700

    Update: Implementasi Room Database dan perbaikan warna UI Daisuke Foodlocks

---
+++ app/src/main/java/com/example/daisukefoddlock10/data/local/AppDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.daisukefoddlock10.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.daisukefoddlock10.data.local.dao.OrderDao
+import com.example.daisukefoddlock10.data.local.entity.OrderEntity
+
+@Database(entities = [OrderEntity::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun orderDao(): OrderDao
+}
